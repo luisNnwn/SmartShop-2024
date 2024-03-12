73092f865996eca57d49239a57458dcd37e69462
Ventura07 <132413286+VenturaCarlos1007@users.noreply.github.com>
2024-03-11 22:16:37 -0600

    First commit

---
+++ admin/admin_accounts.php
@@ -0,0 +1,93 @@
+<?php
+
+include '../components/connect.php';
+
+session_start();
+
+$admin_id = $_SESSION['admin_id'];
+
+if(!isset($admin_id)){
+   header('location:admin_login.php');
+}
+
+if(isset($_GET['delete'])){
+   $delete_id = $_GET['delete'];
+   $delete_admins = $conn->prepare("DELETE FROM `admins` WHERE id = ?");
+   $delete_admins->execute([$delete_id]);
+   header('location:admin_accounts.php');
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <title>Cuentas Admin</title>
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css">
+
+   <link rel="stylesheet" href="../css/admin_style.css">
+
+</head>
+<body>
+
+<?php include '../components/admin_header.php'; ?>
+
+<section class="accounts">
+
+   <h1 class="heading">Cuentas Admin</h1>
+
+   <div class="box-container">
+
+   <div class="box">
+      <p>Añadir nuevo administrador</p>
+      <a href="register_admin.php" class="option-btn">Registrar administrador</a>
+   </div>
+
+   <?php
+      $select_accounts = $conn->prepare("SELECT * FROM `admins`");
+      $select_accounts->execute();
+      if($select_accounts->rowCount() > 0){
+         while($fetch_accounts = $select_accounts->fetch(PDO::FETCH_ASSOC)){   
+   ?>
+   <div class="box">
+      <p> Id. de administrador : <span><?= $fetch_accounts['id']; ?></span> </p>
+      <p> Nombre del administrador : <span><?= $fetch_accounts['name']; ?></span> </p>
+      <div class="flex-btn">
+         <a href="admin_accounts.php?delete=<?= $fetch_accounts['id']; ?>" onclick="return confirm('¿eliminar esta cuenta?')" class="delete-btn">eliminar</a>
+         <?php
+            if($fetch_accounts['id'] == $admin_id){
+               echo '<a href="update_profile.php" class="option-btn">actualizar</a>';
+            }
+         ?>
+      </div>
+   </div>
+   <?php
+         }
+      }else{
+         echo '<p class="empty">¡no hay cuentas disponibles!</p>';
+      }
+   ?>
+
+   </div>
+
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+<script src="../js/admin_script.js"></script>
+   
+</body>
+</html>
---
+++ components/footer.php
@@ -0,0 +1,40 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <h3>Enlaces directos</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> Inicio</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> Sobre Nosotros</a>
+         <a href="shop.php"> <i class="fas fa-angle-right"></i> Comprar</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> Contacto</a>
+      </div>
+
+      <div class="box">
+         <h3>Enlaces adicionales</h3>
+         <a href="user_login.php"> <i class="fas fa-angle-right"></i> Iniciar sesión</a>
+         <a href="user_register.php"> <i class="fas fa-angle-right"></i> Registrarse</a>
+         <a href="cart.php"> <i class="fas fa-angle-right"></i> Carrrito</a>
+         <a href="orders.php"> <i class="fas fa-angle-right"></i> Pedidos</a>
+      </div>
+
+      <div class="box">
+         <h3>Contáctanos.</h3>
+         <a href="tel:9800000000"><i class="fas fa-phone"></i> +503 78164658</a>
+         <a href="tel:9900000000"><i class="fas fa-phone"></i> +503 61257890</a>
+         <a href="mailto:harshchy143@gmail.com"><i class="fas fa-envelope"></i> smartshop@gmail.com</a>
+         <a href="https://www.google.com/myplace"><i class="fas fa-map-marker-alt"></i> San Salvador, El Salvador </a>
+      </div>
+
+      <div class="box">
+         <h3>Síguenos</h3>
+         <a href="https://www.facebook.com/" target="_blank"><i class="fab fa-facebook-f"></i>facebook</a>
+         <a href="https://twitter.com/" target="_blank"><i class="fab fa-twitter"></i>Twitter</a>
+         <a href="https://www.instagram.com/" target="_blank"><i class="fab fa-instagram"></i>Instagram</a>
+      </div>
+
+   </section>
+
+   <div class="credit">&copy; copyright @ <?= date('Y'); ?> by <span>Project Kanban</span> | todos los derechos reservados!</div>
+
+</footer>
